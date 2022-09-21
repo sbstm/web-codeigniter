af60f6e096c88679312c4c1cfb7d252362d60f5e
sbstm <siswa.relative12@upi.edu>
2022-09-21 07:26:24 +0700

    update1

--- public/index.php
+++ public/index.php
@@ -1,67 +1,134 @@
-<?php
+<!doctype html>
+<html lang="en">
 
-// Check PHP version.
-$minPhpVersion = '7.4'; // If you update this, don't forget to update `spark`.
-if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
-    $message = sprintf(
-        'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
-        $minPhpVersion,
-        PHP_VERSION
-    );
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    exit($message);
-}
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 
-// Path to the front controller (this file)
-define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Viga" rel="stylesheet">
 
-// Ensure the current directory is pointing to the front controller's directory
-chdir(FCPATH);
+    <title>SBStm</title>
+</head>
 
-/*
- *---------------------------------------------------------------
- * BOOTSTRAP THE APPLICATION
- *---------------------------------------------------------------
- * This process sets up the path constants, loads and registers
- * our autoloader, along with Composer's, loads our constants
- * and fires up an environment-specific bootstrapping.
- */
+<body>
 
-// Load our paths config file
-// This is the line that might need to be changed, depending on your folder structure.
-require FCPATH . '../app/Config/Paths.php';
-// ^^^ Change this line if you move your application folder
 
-$paths = new Config\Paths();
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">SBS.</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="#">profil</a>
+                    <a class="nav-item nav-link" href="#">about</a>
+                    <a class="nav-item nav-link btn btn-primary text-white tombol" href="#">+</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- akhir Navbar -->
 
-// Location of the framework bootstrap file.
-require rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
+    <!-- pop teks -->
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Selesaikan pekerjaan lebih <span>cepat</span> dan lebih <span>baik</span> bersama
+                <span>kami</span></h1>
+            <a href="https://www.instagram.com/satriabimasakti_12/" class="btn btn-primary tombol">join</a>
+        </div>
+    </div>
 
-// Load environment settings from .env files into $_SERVER and $_ENV
-require_once SYSTEMPATH . 'Config/DotEnv.php';
-(new CodeIgniter\Config\DotEnv(ROOTPATH))->load();
 
-/*
- * ---------------------------------------------------------------
- * GRAB OUR CODEIGNITER INSTANCE
- * ---------------------------------------------------------------
- *
- * The CodeIgniter class contains the core functionality to make
- * the application run, and does all of the dirty work to get
- * the pieces all working together.
- */
+    <!-- container -->
+    <div class="container">
 
-$app = Config\Services::codeigniter();
-$app->initialize();
-$context = is_cli() ? 'php-cli' : 'web';
-$app->setContext($context);
+        <div class="row justify-content-center pb-5">
+            <div class="col-2 info-panel1">
+                <div class="row">
+                    <div class="col-sm justify-content-center">
+                        <h4>Hobbi</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-/*
- *---------------------------------------------------------------
- * LAUNCH THE APPLICATION
- *---------------------------------------------------------------
- * Now that everything is setup, it's time to actually fire
- * up the engines and make this app do its thang.
- */
+        <!-- info panel -->
+        <div class="row justify-content-center pt-5">
+            <div class="col-10 info-panel">
+                <div class="row">
+                    <div class="col-sm">
+                        <img width="70" src="img/film.png" alt="" class="img-fluid float-left mr-3 mt-2">
+                        <h4>Nonton film</h4>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div class="col-lg">
+                        <img width="70" src="img/game.png" alt="" class="img-fluid float-left mr-3 mt-2">
+                        <h4>Main Game</h4>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div class="col-lg">
+                        <img width="70" src="img/volly.png" alt="" class="img-fluid float-left mr-3 mt-2">
+                        <h4>Voli</h4>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-$app->run();
+
+        <!-- kelas -->
+        <div class="row kelas">
+            <div class="col-lg-6">
+                <img src="img/foto kelas.JPG" alt="" class="img-fluid">
+            </div>
+            <div class="col-lg-5">
+                <h2><span>Be</span> assable </h2>
+                <p>Bekerja dengan suasana hati yang lebih asik, menyenangkan dan mempelajari hal baru setiap harinya.</p>
+                <a href="https://drive.google.com/drive/folders/1H_tRY8xnZ1zo_XGeOCOyjdCdV0dw-ISD" class="btn btn-success tombol">Gallery</a>
+            </div>
+        </div>
+
+
+        <section class="footer">
+            <div class="row justify-content-center">
+                <div class="col-lg-8 mutiara">
+                    <p>"Free Fire boleh jadi bukan game HD, tapi kebahagiaan enggak selalu datang dari kemewahan."</p>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-6 justify-content-center d-flex">
+                    <img src="img/kirito.jpg" alt="">
+                    <img src="img/Frame.svg" alt="" class="img-main">
+                    <img src="img/kirito.jpg" alt="">
+                </div>
+            </div>
+            <div class="row justify-content-center info-text">
+                <div class="col-lg text-center akhir">
+                    <h5>Satria Bima Sakti</h5>
+                    <p>Web Development</p>
+                </div>
+            </div>
+        </section>
+    </div>
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
